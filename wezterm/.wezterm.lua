028e37e3c019af640e6d632b84a26327d8b95eff
Achiiv <achi.iv@gmail.com>
2024-10-28 19:09:12 +0100

    on shift+enter send shift+enter -> used in vim keybind to O<Esc>

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,8 +7,10 @@ local config = wezterm.config_builder()
 
 config.default_prog = { "pwsh.exe", "-NoLogo" }
 config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
+config.freetype_load_target = "Light"
+config.freetype_render_target = "HorizontalLcd"
 config.initial_cols = 165
-config.initial_rows = 40
+config.initial_rows = 45
 config.keys = {
 	-- Turn off the default CMD-m Hide action, allowing CMD-m to
 	-- be potentially recognized and handled by the tab
@@ -16,6 +18,11 @@ config.keys = {
 		key = "v",
 		mods = "CTRL",
 		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "Enter",
+		mods = "SHIFT",
+		action = wezterm.action.SendKey({ key = "Enter", mods = "SHIFT" }),
 	},
 	{
 		key = "t",
